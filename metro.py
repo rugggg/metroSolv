fa99e8fbc784d38b10c768587c48e4d03707fef5
Doug Woodward <woodw116@mail.chapman.edu>
2016-11-27 12:17:27 -0800

    Add Batch

--- metro.py
+++ metro.py
@@ -106,7 +106,7 @@ print("reward: ",getReward(state,1,rM))
 
 
 model = Sequential()
-model.add(Dense(120, init='lecun_uniform', input_shape=(4,)))
+model.add(Dense(120, init='lecun_uniform', input_shape=(numStations,)))
 model.add(Activation('relu'))
 #model.add(Dropout(0.2)) I'm not using dropout, but maybe you wanna give it a try?
 
@@ -114,7 +114,7 @@ model.add(Dense(100, init='lecun_uniform'))
 model.add(Activation('relu'))
 #model.add(Dropout(0.2))
 
-model.add(Dense(4, init='lecun_uniform'))
+model.add(Dense(numStations, init='lecun_uniform'))
 model.add(Activation('linear')) #linear output so we can have range of real-valued outputs
 
 rms = RMSprop()
@@ -123,53 +123,76 @@ model.compile(loss='mse', optimizer=rms)
 
 # In[14]:
 
-model.predict(state.reshape(1,4), batch_size=1)
+#model.predict(state.reshape(1,numStations), batch_size=1)
 #just to show an example output; read outputs left to right: up/down/left/right
 
-
-
+model.compile(loss='mse', optimizer=rms)#reset weights of neural network
 epochs = 100
-gamma = 0.9 #since it may take several moves to goal, making gamma high
+gamma = 0.975
 epsilon = 1
+batchSize = 40
+buffer = 80
+replay = []
+#stores tuples of (S, A, R, S')
+h = 0
 for i in range(epochs):
-    
-    state = initState()
     rM = initRewardM()
+    state = initState() #using the harder state initialization function
     status = 1
     #while game still in progress
     while(status == 1):
-        print(state)
         #We are in state S
         #Let's run our Q function on S to get Q values for all possible actions
-        qval = model.predict(state.reshape(1,4), batch_size=1)
+        qval = model.predict(state.reshape(1,numStations), batch_size=1)
         if (random.random() < epsilon): #choose random action
-            action = np.random.randint(0,4)
+            action = np.random.randint(0,numStations)
         else: #choose best action from Q(s,a) values
             action = (np.argmax(qval))
         #Take action, observe new state S'
         new_state = makeMove(state, action, rM)
         #Observe reward
         reward = getReward(new_state, action, rM)
-        #Get max_Q(S',a)
-        newQ = model.predict(new_state.reshape(1,4), batch_size=1)
-        maxQ = np.max(newQ)
-        y = np.zeros((1,4))
-        y[:] = qval[:]
-        if reward != 10: #non-terminal state
-            update = (reward + (gamma * maxQ))
-        else: #terminal state
-            print("-__--___-terminal-___-_-___-_")
-            update = reward
-        y[0][action] = update #target output
-        print("y: ",y)
-        print("Game #: %s" % (i,))
-        model.fit(state.reshape(1,4), y, batch_size=1, nb_epoch=1, verbose=1)
-        state = new_state
-        if reward == 10:
+        
+        #Experience replay storage
+        if (len(replay) < buffer): #if buffer not filled, add to it
+            replay.append((state, action, reward, new_state))
+        else: #if buffer full, overwrite old values
+            if (h < (buffer-1)):
+                h += 1
+            else:
+                h = 0
+            replay[h] = (state, action, reward, new_state)
+            #randomly sample our experience replay memory
+            minibatch = random.sample(replay, batchSize)
+            X_train = []
+            y_train = []
+            for memory in minibatch:
+                #Get max_Q(S',a)
+                old_state, action, reward, new_state = memory
+                old_qval = model.predict(old_state.reshape(1,numStations), batch_size=1)
+                newQ = model.predict(new_state.reshape(1,numStations), batch_size=1)
+                maxQ = np.max(newQ)
+                y = np.zeros((1,numStations))
+                y[:] = old_qval[:]
+                if reward == -1: #non-terminal state
+                    update = (reward + (gamma * maxQ))
+                else: #terminal state
+                    update = reward
+                y[0][action] = update
+                X_train.append(old_state.reshape(numStations,))
+                y_train.append(y.reshape(numStations,))
+            
+            X_train = np.array(X_train)
+            y_train = np.array(y_train)
+            print("Game #: %s" % (i,))
+            model.fit(X_train, y_train, batch_size=batchSize, nb_epoch=1, verbose=1)
+            state = new_state
+        if reward != 10: #if reached terminal state, update game status
             status = 0
         clear_output(wait=True)
-    if epsilon > 0.1:
+    if epsilon > 0.1: #decrement epsilon over time
         epsilon -= (1/epochs)
+
 
 
 # In[16]:
@@ -205,4 +228,6 @@ def testAlgo():
 # In[30]:
 
 testAlgo()
-
+testAlgo()
+testAlgo()
+testAlgo()
