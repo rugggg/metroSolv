0f4c2e414b7ffde43ca6adf2f509db8f6945f2d2
Doug Woodward <woodw116@mail.chapman.edu>
2016-11-27 22:23:35 -0800

    working! with small

--- createMatrix.py
+++ createMatrix.py
@@ -4,16 +4,20 @@ import random
 numStations = 10
 m = np.full((numStations,numStations),-10)
 
-tube = np.genfromtxt('londonUnderground.csv',delimiter=',')
+tube = np.genfromtxt('small.csv',delimiter=',')
 print(tube)
 
 
 
 col = tube[:,0]
+row  = tube[0,:]
+
 
 for i in range (0,len(col)):
     if col[i] == -1:
         col[i] = 10
+    if row[i] == -1:
+        row[i] = 10
 
 print(tube)
 
